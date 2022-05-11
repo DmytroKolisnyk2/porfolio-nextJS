1903e18f97364f1d1c3e09d27ef979235d5ee2af
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-05-11 10:07:30 +0300

    add next

---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <header>dodo </header>
+      <main>
+        <section>dodo </section>
+        <section>dodo </section>
+        <section>dodo </section>
+        <section>dodo </section>
+        <section>dodo </section>
+      </main>
+      
+      <footer>dodo </footer>
+    </>
+  );
+}
